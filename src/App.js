304fdd1cdb3459b957d8e6f93e0579d8cbaf0628
Dayeon1233 <swdy1233@gmail.com>
2021-12-02 11:23:39 +0900

    sass사용해보기

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import react, {Component} from 'react';
+import SassComponent from './SassComponent';
+
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <SassComponent/>
+      </div>
+    );
+  }
+}
+
+
+export default App;
