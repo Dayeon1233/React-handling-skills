2173c3df617ad181ea79f8ed6c2f8989a333b318
Dayeon1233 <swdy1233@gmail.com>
2021-12-03 13:51:55 +0900

    styled-components 실습

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import react, {Component} from 'react';
-import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 
 class App extends Component{
   render(){
     return(
       <div>
-        <CSSModule/>
+        <StyledComponent/>
       </div>
     );
   }
